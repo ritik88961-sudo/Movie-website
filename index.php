3c44bdb22abbfb87bff1176fe1b1ae69e0ac3fe0
ritik88961-sudo <115653134+ritik88961-sudo@users.noreply.github.com>
2024-06-11 22:09:55 +0530

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
-    include_once('./views/templates/header.php');
+    // Include the header template
+    if (!@include_once('./views/templates/header.php')) {
+        echo "Error including header.php";
+    }
 ?>
 <main>
-    <?php include_once('./views/movie.php'); ?>
+    <?php
+        // Include the main movie content
+        if (!@include_once('./views/movie.php')) {
+            echo "Error including movie.php";
+        }
+    ?>
 </main>
-<?php include_once('./views/templates/footer.php') ?>
+<?php
+    // Include the footer template
+    if (!@include_once('./views/templates/footer.php')) {
+        echo "Error including footer.php";
+    }
+?>
